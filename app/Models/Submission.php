bfcc9747dcf1b22ff128e230e457175711bb8198
Darren Coutts <darren.coutts@interhubdigital.com>
2019-09-06 20:45:57 +0100

    Setup metable model

--- app/Models/Submission.php
+++ app/Models/Submission.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Kodeine\Metable\Metable;
 
 class Submission extends Model
 {
     //
+    use Metable;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $with = [
+        'metas',
+    ];
 }
