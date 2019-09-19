ac83bc9dc935c7fa40ee10a4af64094153c080fa
Darren Coutts <darren.coutts@abdn.ac.uk>
2019-09-19 15:59:04 +0100

    tests

--- tests/Unit/FieldTest.php
+++ tests/Unit/FieldTest.php
@@ -42,8 +42,104 @@ class FieldTest extends TestCase
         // when a field is created
         factory(Field::class, 5)->create(['form_id' => $form->id]);
 
-        // it should be order one
+        // it should have the next
         $field = Field::latest('id')->first();
         $this->assertEquals(5, $field->order);
+
+        $field = factory(Field::class)->create(['form_id' => $form->id]);
+        $this->assertEquals(6, $field->fresh()->order);
+    }
+
+    public function test_a_field_can_be_moved_up()
+    {
+        // given a form
+        $form = factory(Form::class)->create();
+
+        // that has two fields
+        factory(Field::class, 2)->create(['form_id' => $form->id]);
+
+        // the second field
+        $field = Field::latest('id')->first();
+
+        // can be moved above the first
+        $field->moveOrderUp();
+
+        // and therefore its order is 1
+        $this->assertEquals(1, $field->fresh()->order);
+    }
+
+    public function test_a_field_can_be_moved_down()
+    {
+        // given a form
+        $form = factory(Form::class)->create();
+
+        // that has two fields
+        factory(Field::class, 2)->create(['form_id' => $form->id]);
+
+        // the first field
+        $field = Field::oldest('id')->first();
+
+        // can be moved below the second
+        $field->moveOrderDown();
+
+        // and therefore its order is 2
+        $this->assertEquals(2, $field->fresh()->order);
+    }
+
+    public function test_the_top_item_cant_be_moved_any_higher()
+    {
+        // given a form
+        $form = factory(Form::class)->create();
+
+        // that has two fields
+        factory(Field::class, 2)->create(['form_id' => $form->id]);
+
+        // the first field
+        $field = Field::oldest('id')->first();
+
+        // cant be moved up
+        $field->moveOrderUp();
+
+        // and therefore its order is still 1
+        $this->assertEquals(1, $field->fresh()->order);
+    }
+
+    public function test_the_bottom_item_cant_be_moved_any_lower()
+    {
+        // given a form
+        $form = factory(Form::class)->create();
+
+        // that has two fields
+        factory(Field::class, 2)->create(['form_id' => $form->id]);
+
+        // the second field
+        $field = Field::latest('id')->first();
+
+        // cant be moved down
+        $field->moveOrderDown();
+
+        // and therefore its order is still 1
+        $this->assertEquals(2, $field->fresh()->order);
+    }
+
+    public function test_the_order_can_be_retrived()
+    {
+        // given a form
+        $form = factory(Form::class)->create();
+
+        // that has two fields
+        factory(Field::class, 2)->create(['form_id' => $form->id]);
+
+        // the second field
+        $field = Field::latest('id')->first();
+
+        // is then moved above the first
+        $field->moveOrderUp();
+
+        // therefore the order
+        $fields = Field::ordered()->get();
+
+        // is updated to reflect this
+        $this->assertTrue($fields[0]->is($field));
     }
 }
