43b850a6cf981679175d0a1bc8414c54650497fc
Darren Coutts <darren.coutts@interhubdigital.com>
2019-10-01 18:08:16 +0100

    restructure tests

--- tests/Feature/Middleware/AuthenticationMiddlewareTest.php
+++ tests/Feature/Middleware/AuthenticationMiddlewareTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class AuthenticationTest extends TestCase
+class AuthenticationMiddlewareTest extends TestCase
 {
     use RefreshDatabase;
 
